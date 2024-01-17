7b73f0ac0524d7c934d2b16c21782fa5f89670d6
Kacper <kacper.szatan@gmail.com>
2024-01-17 12:26:35 +0100

    nvim: split init.lua to modular approrach
    Use mess.lua for checking stuff out.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,27 +1,2 @@
-local vim = vim
-local api = vim.api
-
-
--- Behaviour
------------------------------------------------------------------------- 
--- TODO: @Kacper
--- * strip trailing whitespaces function
--- * trigger stripping function on write
-
--- Appearance
------------------------------------------------------------------------- 
--- relative line numbers
-vim.wo.relativenumber = true
--- absolute line number on the cursor line
-vim.wo.number = true
--- show a bar on column 80 and 120
-vim.wo.colorcolumn = "80,120"
--- set color of colorcolumns
-vim.highlight.create('ColorColumn', {ctermbg=1, guibg=Red}, false)
--- tab eq 4 spaces
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
-vim.o.smarttab = true
--- soft ignore case in searches
-vim.o.ignorecase = true
-vim.o.smartcase = true
+require "user.options"
+require "user.mess"
