dfdf4829f2fdd65053cec6f3b9035fd52dfa536c
Kacper <kacper.szatan@gmail.com>
2024-01-25 14:24:11 +0100

    nvim: update TODO list and change few opt

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -1,13 +1,22 @@
+-- Extensions
+------------------------------------------------------------------------
+-- TODO: @Kacper
+-- * treesitter
+-- * LSP
+-- * fuzzy finder
+-- *
+
 
 -- Behaviour
------------------------------------------------------------------------- 
+------------------------------------------------------------------------
 -- TODO: @Kacper
 -- * strip trailing whitespaces function
--- * trigger stripping function on write
+-- * trigger stripping whitespace function on write
 -- * auto trigger clang-format on *.h *.cpp *.c *.hpp files
 
+
 -- Appearance
------------------------------------------------------------------------- 
+------------------------------------------------------------------------
 
 -- show a bar on column 80 and 120
 vim.wo.colorcolumn = "80,120"
@@ -17,12 +26,15 @@ vim.highlight.create('ColorColumn', {ctermbg=1, guibg=Red}, false)
 -- :help options
 local options = {
     backup = false,                          -- creates a backup file
+    -- undodir = os.getenv(HOME) .. /.vim/undodir,  -- undodir & undofile let you undo forever
+    -- undofile = true,
     clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
-    cmdheight = 2,                           -- more space in the neovim command line for displaying messages
+    cmdheight = 1,                           -- if set to 2 there is more space in the neovim command line for displaying messages
     completeopt = { "menuone", "noselect" }, -- mostly just for cmp
     conceallevel = 0,                        -- so that `` is visible in markdown files
     fileencoding = "utf-8",                  -- the encoding written to a file
-    hlsearch = true,                         -- highlight all matches on previous search pattern
+    hlsearch = false,                         -- highlight all matches on previous search pattern
+    incsearch = true,
     ignorecase = true,                       -- ignore case in search patterns
     mouse = "a",                             -- allow the mouse to be used in neovim
     pumheight = 10,                          -- pop up menu height
@@ -36,11 +48,12 @@ local options = {
     termguicolors = true,                    -- set term gui colors (most terminals support this)
     timeoutlen = 1000,                       -- time to wait for a mapped sequence to complete (in milliseconds)
     undofile = true,                         -- enable persistent undo
-    updatetime = 300,                        -- faster completion (4000ms default)
+    updatetime = 300,                        -- faster completion (4000ms default) Maybe set even to 50?
     writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
     expandtab = true,                        -- convert tabs to spaces
     shiftwidth = 4,                          -- the number of spaces inserted for each indentation
-    tabstop = 4,                             -- insert 2 spaces for a tab
+    tabstop = 4,                             -- insert 4 spaces for a tab
+    softtabstop = 4,
     smarttab = true,
     cursorline = true,                       -- highlight the current line
     number = true,                           -- set numbered lines
@@ -52,7 +65,6 @@ local options = {
     sidescrolloff = 8,
     guifont = "monospace:h17"               -- the font used in graphical neovim applications
 }
-
 vim.opt.shortmess:append "c"
 
 for k, v in pairs(options) do
