83d55fdcb450750d0830c05f45054c5b01eacd7c
Kacper <kacper.szatan@gmail.com>
2024-02-10 19:13:40 +0100

    zsh: add aliases

---
+++ shell-settings/user-aliases.zsh
@@ -0,0 +1,6 @@
+# datamine project virtual-env activation
+alias datamine-venv="cd && source ./workspace/NDUS3/datamine/.venv/bin/activate && cd -"
+alias ndus3-ssh-server="kacpers@192.168.88.90"
+
+# STM32 Cube IDE shortcut
+alias stm-ide="/opt/st/stm32cubeide_1.14.0/stm32cubeide"
