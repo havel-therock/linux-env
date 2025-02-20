03a5046929c5dddaa36db8922627f1184b3d0c62
Kacper Szatan <kacper.szatan@gmail.com>
2025-02-20 13:09:38 +0100

    shell: add file for export variables and change some aliases

--- shell-settings/user-aliases.zsh
+++ shell-settings/user-aliases.zsh
@@ -1,8 +1,5 @@
 # STM32 Cube IDE shortcut
 # alias stm-ide="/opt/st/stm32cubeide_1.14.0/stm32cubeide"
 
-# vim alias as nvim
-# alias vim="nvim"
-
 # battery_check
 alias batt_check="cat /sys/class/power_supply/BAT0/capacity"
