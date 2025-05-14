500c967080e6c8712aa67b5e8c3073e0a66cec28
Kacper <kacper.szatan@gmail.com>
2025-05-14 10:59:28 +0200

    nvim: add link to old glibc neovim versions

--- scripts/install_neovim.sh
+++ scripts/install_neovim.sh
@@ -2,6 +2,8 @@
 mkdir -p ${HOME}/tools/
 cd ${HOME}/tools/
 
+# version for older glibc
+# https://github.com/neovim/neovim-releases/releases/download/v0.11.1/nvim-linux-x86_64.tar.gz
 wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim-linux64.tar.gz
 tar xzvf nvim-linux64.tar.gz
 rm nvim-linux64.tar.gz
